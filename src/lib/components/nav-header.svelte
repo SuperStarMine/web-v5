a656e7e211d751192ccfca4c116293b980270567
HIBIKI <hibiki.apple.1984@gmail.com>
2022-12-10 19:30:17 +0900

    Add: NavHeader

---
+++ src/lib/components/nav-header.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  export let targets: { label: string; id: string }[];
+</script>
+
+<header>
+  <img class="logo" src="ssm-logo-landscape.svg" alt="" />
+  <button>
+    メニュー
+    <div class="arrow">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+        <path d="M12 2 4.5 20.29l.71.71L12 18l6.79 3 .71-.71z" />
+      </svg>
+    </div>
+  </button>
+</header>
+
+<style lang="stylus">
+  header
+    box-sizing border-box
+    display flex
+    align-items center
+    justify-content space-between
+    width 100%
+    height 4rem
+    padding 0 (@height / 2)
+    background-color #fff
+  .logo
+    height 60%
+  button
+    display flex
+    align-items center
+    height 100%
+    padding 0
+    margin 0
+    font-size 1.2em
+    background-color #fff0
+    border none
+    color #333
+  .arrow
+    box-sizing border-box
+    display grid
+    place-items center
+    height 55%
+    aspect-ratio 1
+    margin-left 1ch
+    background-color #f7931e
+    border-radius 40%
+    svg
+      height 50%
+      fill #fff
+      rotate 45deg
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation.</p>
+<script lang="ts">
+  import NavHeader from '$lib/components/nav-header.svelte';
+</script>
+
+<NavHeader
+  targets={[
+    { label: '作品', id: '#works' },
+    { label: 'ニュース', id: '#news' },
+    { label: 'チーム', id: '#about' },
+    { label: '連絡先', id: '#contact' },
+  ]}
+/>
+
+
+<style lang="stylus">
+  :global(body)
+    background-color #e5e5e5
+</style>
