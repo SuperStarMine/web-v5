e442fbdb317c7dbb4ab191acf1d9cbcf8214a985
HIBIKI <hibiki.apple.1984@gmail.com>
2022-12-11 00:14:47 +0900

    Add: Nav icon rotation gimmick

--- src/lib/components/nav-header.svelte
+++ src/lib/components/nav-header.svelte
@@ -1,13 +1,32 @@
 <script lang="ts">
   export let targets: { label: string; id: string }[];
+
+  let arrow: HTMLElement;
+  let angle = 0;
+
+  function handleMouse(event: { clientX: number; clientY: number }) {
+    const rect = arrow.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    angle = (Math.atan2(centerX - event.clientX, event.clientY - centerY) * 180) / Math.PI + 180;
+    angle = Math.min(270, Math.max(135, angle));
+  }
 </script>
+
+<svelte:body on:mousemove={handleMouse} />
 
 <header>
   <img class="logo" src="ssm-logo-landscape.svg" alt="" />
   <button>
     メニュー
-    <div class="arrow">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <div class="arrow" bind:this={arrow}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        style="rotate: {angle}deg"
+      >
         <path d="M12 2 4.5 20.29l.71.71L12 18l6.79 3 .71-.71z" />
       </svg>
     </div>
@@ -33,9 +52,9 @@
     padding 0
     margin 0
     font-size 1.2em
+    color #333
     background-color #fff0
     border none
-    color #333
   .arrow
     box-sizing border-box
     display grid
