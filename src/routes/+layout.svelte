0625ec86f303a4031c08fac588de162bdcf2b116
HIBIKI <hibiki.apple.1984@gmail.com>
2022-12-10 17:11:05 +0900

    Add: Basic web setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { LayoutData } from './$types';
+
+  export let data: LayoutData;
+</script>
+
+<slot />
+
+<style lang="stylus">
+  :global(html, body)
+    margin: 0
+    width: 100%
+    height: 100%
+</style>
